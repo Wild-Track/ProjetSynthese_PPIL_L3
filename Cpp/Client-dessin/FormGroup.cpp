6e1e70444f5699fc34b3e9c34df894e0217acbc6
Wildryx <Wildryx@outlook.fr>
2022-03-05 11:57:27 +0100

    Add FormGroup

---
+++ Cpp/Client-dessin/FormGroup.cpp
@@ -0,0 +1,65 @@
+#include "FormGroup.h"
+
+
+FormGroup::FormGroup(string name, string color): Form(color)
+{
+	_name = name;
+}
+
+void FormGroup::addForm(Form& form)
+{
+	if (form.getFormGroup() == NULL)
+	{
+		_formList.push_back(&form);
+		form.setFormGroup(this);
+	}
+}
+
+void FormGroup::deleteForm(int i)
+{
+	Form* form = _formList[i];
+	form->setFormGroup(NULL);
+	_formList.erase(_formList.begin() + i);
+}
+
+void FormGroup::translation(const Vecteur2D& u)
+{
+	for (Form* form : _formList)
+	{
+		(*form).Form::translation(u);
+	}
+}
+
+void FormGroup::rotation(const Vecteur2D& invariantPoint, const double& rad)
+{
+	for (Form* form : _formList)
+	{
+		(*form).Form::rotation(invariantPoint, rad);
+	}
+}
+
+void FormGroup::homothety(double ratio, const Vecteur2D& invariant)
+{
+	for (Form* form : _formList)
+	{
+		(*form).Form::homothety(ratio, invariant);
+	}
+}
+
+bool FormGroup::operator ==(const FormGroup& group)
+{
+	return group._color == _color && group._name == _name;
+}
+
+FormGroup::operator string() const
+{
+	string out = "{";
+
+	for (Form* form : _formList)
+	{
+		out += string(*form);
+	}
+	out += this->Form::_color;
+	out += "}";
+	return out;
+}
---
+++ Cpp/Client-dessin/FormGroup.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Form.h"
+
+using namespace std;
+
+class FormGroup: public Form
+{
+private:
+	vector<Form*> _formList;
+	string _name;
+
+public:
+
+	FormGroup(string name, string color);
+
+	void addForm(Form& form);
+	void deleteForm(int i);
+
+	void translation(const Vecteur2D& u);
+	void rotation(const Vecteur2D& invariantPoint, const double& rad);
+	void homothety(double ratio, const Vecteur2D& invariant);
+
+	bool operator ==(const FormGroup& group);
+	operator string() const;
+};
+
